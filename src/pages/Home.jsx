c12ddf74c9e82d4b13d4288c9dca5234849d9414
Vikrant Mulankar <vikranmulankar389@gmail.com>
2025-08-03 22:56:50 +0530

    some change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,62 +18,7 @@ const Home = () => {
   return (
     <>
     
-      {/* <div className="home-banner-container">
-        <svg
-          className="heroCurve"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 1440 320"
-          preserveAspectRatio="none"
-        >
-          <path
-            fill="#ffffff"
-            fillOpacity="1"
-            d="M0,140 C360,260 1200,450 1440,150 L1440,318 L0,320 Z"
-          />
-        </svg>
 
-        <div className="home-banner-wrapper">
-          <div className="home-banner-left">
-            <div className="home-banner-name">MY CITY APP</div>
-            <div className="home-banner-heading">
-              Discover, Experience, <br className="home-text-br" /> Engage
-            </div>
-
-            <p className="home-banner-para">
-              Digitize your town's tourism and culture with an interactive
-              <br className="home-text-br" /> mobile platform.
-              <br className="home-text-br" />
-              <br className="home-text-br" />
-              <br className="home-text-br" />
-              From storytelling and events to local business promotions
-              <br className="home-text-br" /> — all in one app.
-            </p>
-
-            <div className="home-banner-button-container">
-              <button className="home-book-btn">Book a Free Demo</button>
-              <button className="home-contact-btn">
-                Contact Maverick Mount
-              </button>
-            </div>
-          </div>
-
-          <div className="home-banner-right">
-            <img src={homeImg} alt="homeImg" />
-            <div className="home-my-city-app-img">
-              <img src={myCityAppImg} alt="myCityAppImg" />
-            </div>
-            <div className="home-place-app-img">
-              <img src={homePlace} alt="homePlace" />
-            </div>
-            <div className="home-sleeping-app-img">
-              <img src={SleepingImg} alt="SleepingImg" />
-            </div>
-          </div>
-        </div>
-        <div className="home-vector-img">
-          <img src={vector} alt="vector" />
-        </div>
-      </div> */}
 
 <div className={styles.homeBannerContainer}>
   <svg
--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -9,7 +9,7 @@ import interactive from "../assets/icons/interactive.png";
 import rewards from "../assets/icons/rewards.png";
 import mobile from "../assets/icons/mobile.png";
 import admin from "../assets/icons/admin.png";
-import Vector from "../assets/icons/Vector.png";
+import vector from "../assets/icons/vector.png";
 
 const Overview = () => {
   return (
@@ -21,7 +21,7 @@ const Overview = () => {
       </h4>
       <div className={styles.overviewWrapper}>
         <div className={styles.cornerBadge}>
-          <img src={Vector} alt="Vector" />
+          <img src={vector} alt="vector" />
         </div>
 
         <div className={styles.appContainer}>
